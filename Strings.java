e6dfcd5af04f5fc3a63746b3c0452c490518c6a0
harish-h21 <52001801+harish-h21@users.noreply.github.com>
2019-06-19 07:20:37 -0700

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -24,31 +24,50 @@ public class Strings {
         String str="hi 2 all... this is istring test 1";
         String str1="hi 2 all... this is istring test 1";
         String str2="Hi 2 alL... ThiS is iString Test 1";
+        
+        //q1 compare strings with and without ignoring case
         System.out.println(str.compareTo(str2)+" "+str.compareToIgnoreCase(str2));
+        
+        //q2 -no of occurance of a string
         System.out.println("Number of occurance of is:"+(str.split("is").length-1));
+        
+        //q11 equals and == 
         System.out.println(str2==str);
         System.out.println(str2.equals(str));
+        //eliminate all numbers
         str1=str1.replaceAll("[0-9]","");
         System.out.println(str1);
+        //eliminate all given substrings in a string
         str1=str1.replaceAll("is", "");
         System.out.println(str1);
+        
+        //replace a char by other char
         str1=str1.replaceAll("t","x");
         System.out.println(str1);
-        StringTokenizer st=new StringTokenizer(str2,"i",true);
-        String[] arr=str2.split("i");
+        //split string
+          String[] arr=str2.split("i");
         for(String s:arr)
         {
             System.out.println(s);
         }
+        //tokenize string with delimeter
+        StringTokenizer st=new StringTokenizer(str2,"i",true);
+      
         while(st.hasMoreTokens())
         {
             System.out.println(st.nextToken());
         }
+        
+        //print current date
        Date date=new Date();
+           SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yy");
+         System.out.println(sdf.format(date));
+        
+        //compare two dates
        System.out.println("Enter date");
        String d=sc.nextLine();
-       SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yy");
-       System.out.println(sdf.format(date));
+    
+      
         try {
             if(sdf.parse(d).compareTo(sdf.parse(sdf.format(date)))<0)
             {
